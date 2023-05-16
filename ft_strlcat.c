0ff5a236fe46d44849cfa06c582a48289eb54283
Roberto <rpliego@car15s2.42barcelona.com>
2023-05-16 19:57:41 +0200

    strlcat ya corregido

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,36 +14,40 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	dstlen;
-	size_t	srclen;
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	total_len;
+	size_t	copy_len;
 	size_t	i;
 
-	dstlen = 0;
-	srclen = 0;
+	dest_len = 0;
+	src_len = 0;
 	i = 0;
-	while (dstlen <= size && dest[dstlen] != '\0')
-		dstlen++;
-	while (src[srclen] != '\0')
-		srclen++;
-	i = dstlen;
-	while (i < size - 1 && *src != '\0')
+	while (dest[dest_len] != '\0' && dest_len < size)
+		dest_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	total_len = dest_len + src_len;
+	if (dest_len >= size)
+		return (size + src_len);
+	copy_len = size - dest_len - 1;
+	while (src[i] != '\0' && i < copy_len)
 	{
-		dest[i] = *src;
-		src++;
+		dest[dest_len + i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	if (dstlen > size)
-		return (size + srclen);
-	else
-		return (dstlen + srclen);
+	dest[dest_len + i] = '\0';
+	return (total_len);
 }
+
+// #include <stdio.h>
+// #include <string.h>
 
 // int	main(void)
 // {
-// 	char	stri[] = "the cake is a lie !\0I'm hidden lol\r\n";
-// 	char	stri2[] = "there is no stars in the sky";
+// 	char	stri[] = "lorem ipsum dolor sit amet";
+// 	char	stri2[] = "r";
 
-// 	printf("%zu\n", strlcat(stri2, stri, 0));
-// 	printf("%zu\n", ft_strlcat(stri2, stri, 0));
+// 	printf("%zu\n", strlcat(stri2, stri, 5));
+// 	printf("%zu\n", ft_strlcat(stri2, stri, 5));
 // }
